6ec57ac4a8c2ca3036f4859154c3df4ae296984f
Roman Tashev <rzet777@gmail.com>
2021-02-02 14:40:58 +0300

    Lesson2

--- 2/Switch/Program.cs
+++ 2/Switch/Program.cs
@@ -10,6 +10,20 @@ namespace Switch
     {
         static void Main(string[] args)
         {
+            int s = 30;
+            switch (s) //goto
+            {
+                case 1: /*действие*/;
+                    break;//go from switch
+                case 2: /*действие*/; ;
+                    break;//go from switch
+                case 4: /*действие*/;
+                    break;//go from switch
+                case 5: /*действие*/;
+                    break;//go from switch
+                default:/*действие*/;
+                    break;//go from switch
+            }
         }
     }
 }
--- 2/Tern/Program.cs
+++ 2/Tern/Program.cs
@@ -8,6 +8,12 @@ namespace Tern
 {
     class Program
     {
+        static int Max(int a, int b) 
+        {
+            /*if (a > b) return a;
+            else return b;*/
+            return a > b ? a : b;
+        }
         static void Main(string[] args)
         {
             Console.WriteLine("Ваедите число:");
