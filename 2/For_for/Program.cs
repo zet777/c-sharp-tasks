abfd6ac4920d181543a64973c17fbb69cb83fac2
Roman Tashev <rzet777@gmail.com>
2021-02-17 23:07:11 +0300

    For_for

--- 2/For_for/Program.cs
+++ 2/For_for/Program.cs
@@ -10,6 +10,16 @@ namespace For_for
     {
         static void Main(string[] args)
         {
+            for (int i = 0; i < 80; i++)
+            {
+                for (int j = 0; j < 24; j++)
+                {
+                    Console.SetCursorPosition(i, j);
+                    Console.Write("$");
+                    System.Threading.Thread.Sleep(100);
+                }
+                Console.ReadKey();
+            }
         }
     }
 }
